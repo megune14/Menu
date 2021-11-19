1f7a8ec24cc3dd9e3d1f65c29a9b9111fd95455e
megune14 <megune1412@gmail.com>
2021-11-19 13:22:35 +0900

    cssへのルート変更

--- Menu/resources/views/html/UserInput.blade.php
+++ Menu/resources/views/html/UserInput.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../CSS/MOBILEbase.css">
+  <link rel="stylesheet" href="../../css/MOBILEbase.css">
   <title>顧客情報入力</title>
 </head>
 
