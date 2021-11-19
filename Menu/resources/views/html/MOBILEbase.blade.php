ea9c8ee253b98550e5836b7dcd10f571f49ff530
megune14 <megune1412@gmail.com>
2021-11-19 13:44:21 +0900

    cssの階層変更

--- Menu/resources/views/html/MOBILEbase.blade.php
+++ Menu/resources/views/html/MOBILEbase.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../css/MOBILEbase.css">
+  <link rel="stylesheet" href="{{ asset('dist/scc/MOBAILEbase.css') }}">
   <title>@yield('title')</title>
 </head>
 
