e5e6b8d998eefd7d2f919732d810f8512084be92
yama-2312 <mit2071002@stu.o-hara.ac.jp>
2021-11-24 15:28:21 +0900

    顧客情報のDBをつなげて表示したよ（生年月日以外）

--- Menu/resources/views/html/UserInfo.blade.php
+++ Menu/resources/views/html/UserInfo.blade.php
@@ -3,8 +3,18 @@
 @section('title','顧客情報')
 
 @section('contents')
-<h1>顧客情報</h1>
-
-
-
+  <h1>顧客情報</h1>
+    
+  <?php
+    $info = \DB::table('UserInfodb')->get();    
+   foreach ($info as $info) {
+   }
+  
+    echo '<p>名前：' . $info->name   ."</p>\n";
+    echo '<p>メールアドレス：' . $info->email   ."</p>\n";
+    SELECT date_format(now(), '%Y/%m/%d');
+    echo '<p>生年月日：' . $info->birthday  ."</p>\n";
+    echo '<p>現在のポイント：' . $info->point   .'pt'."</p>\n";
+    ?>
+  
 @endsection
