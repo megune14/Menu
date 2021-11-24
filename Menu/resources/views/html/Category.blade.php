49fbc8748f216a8b30837b7efed62db04e064e01
megune14 <megune1412@gmail.com>
2021-11-24 13:53:16 +0900

    categoryの作成

--- Menu/resources/views/html/Category.blade.php
+++ Menu/resources/views/html/Category.blade.php
@@ -1,1 +1,19 @@
 @extends('html.MOBILEbase')
+<?php
+$items = \DB::table('test')->get();
+
+?>
+
+
+
+  @section('title','メール送信完了')
+  @foreach ($items as $item)
+    <p>{{ $item->category}}</p>
+
+  @endforeach
+
+    <!--テキストサンプル-->
+    @section('contents')
+
+    <!--/テキストサンプル-->
+    @endsection
