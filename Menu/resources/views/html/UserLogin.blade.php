ca0b350d533baaac43948a48efeaa3d6af880f7e
shio-noodle <shiohata71156@yahoo.co.jp>
2021-11-25 11:52:40 +0900

    ログイン画面のcss追加(未完成)

--- Menu/resources/views/html/UserLogin.blade.php
+++ Menu/resources/views/html/UserLogin.blade.php
@@ -7,21 +7,22 @@
 
     <!--テキストサンプル ここがアタイの仕事-->
     @section('contents')
-      <div class="LoginForm">
+      <div class="loginform">
       
         <form action="" method="post">
-          <p>メールアドレス<br>
-            <input type="text" name="name"></p>
-          <p>パスワード</p>
-            <input type="password" name="textbox" size="30" maxlength="8"><br>
+          <div class="loginform_btm">
+            <input type="text" name="name" placeholder="メールアドレス"></p>
+          
+            <input type="password" name="textbox" placeholder="パスワード" size="30" maxlength="8"><br>
+          </div>          
   
-            <input type="checkbox" name=“sample” value="1">ログイン状態を保持する<br>
-            
+          <input type="checkbox" name=“sample” value="1">ログイン状態を保持する<br>
+          
           <a href="#">新規登録</a><br>
           <a href="#">パスワードを忘れた場合</a><br>
           <a href="#">店舗用</a>
   
-          <p><input type="submit" value="ログイン"></p>
+          <button type="submit">ログイン</button>
   
         </form>          
       </div>
