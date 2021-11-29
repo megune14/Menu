434c9be845c88152b6d659b22f33a2c0131e4e14
yama-2312 <mit2071002@stu.o-hara.ac.jp>
2021-11-29 13:06:36 +0900

    html修正

--- Menu/resources/views/html/PassReSetting.blade.php
+++ Menu/resources/views/html/PassReSetting.blade.php
@@ -13,12 +13,12 @@
         <form action="" method="post">
           
           <p>新規パスワード</p>
-          　<input type="password" name="textbox" size="30" maxlength="8"><br>
+          　<input type="password" name="textbox" size="30" minlength="8"><br>
 
           <p>新規パスワード(確認用)</p>
-          　<input type="password" name="textbox" size="30" maxlength="8"><br>  
+          　<input type="password" name="textbox" size="30" minlength="8"><br>  
             
-          <p><input type="submit" value="戻る">
+          <button type="button" onclick=history.back()>戻る</button>
           <input type="submit" class="ChangePass" value="変更">
   
         </form>          
