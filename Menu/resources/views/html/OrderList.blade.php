19dd67eca795ff45ad40e76290f3df2fe8fee65d
shio-noodle <shiohata71156@yahoo.co.jp>
2021-11-29 13:42:47 +0900

    オーダーリストの確認

--- Menu/resources/views/html/OrderList.blade.php
+++ Menu/resources/views/html/OrderList.blade.php
@@ -11,7 +11,7 @@
       </span>
       <span class="menu-text">
       <span class="m-name">醤油ラーメン</span>
-      <span class="m-price">800円</span>
+      <span class="m-price">800円</span><br>
       <input type="number" value="1" min="1" max="10" step="1">
       <button type="submit" name="delete_btn" value="1">削除</button>
   </li> 
