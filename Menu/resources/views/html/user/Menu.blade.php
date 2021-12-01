13013bb20591752158fcabcc30607859f2707c41
shio-noodle <shiohata71156@yahoo.co.jp>
2021-12-01 14:15:02 +0900

    Menuの完成

--- Menu/resources/views/html/user/Menu.blade.php
+++ Menu/resources/views/html/user/Menu.blade.php
@@ -13,8 +13,14 @@ $item = Menu::find($id);
 
 
   @section('contents')
+    <div class="category">
+      <h1>メニュー一覧</h1>
+    </div>
+
     <div class="card">
          <img src="/images/test2.png">
          <p>{{ $item->name}}</p>
-         <p>{{ $item->price}}</p>
+         <p>{{ $item->price}}円</p>
     </div>
+
+  @endsection
