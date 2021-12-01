4971d94d479330eccffa20964e0ca704b0a5d3bf
megune14 <megune1412@gmail.com>
2021-12-01 13:45:01 +0900

    会員登録機能の追加

--- Menu/app/Models/User.php
+++ Menu/app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
         'email',
         'password',
+        'birthday',
     ];
 
     /**
