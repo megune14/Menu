26806cbedfd3a9f3c684fafc6666831bd8aab67c
yama-2312 <mit2071002@stu.o-hara.ac.jp>
2021-12-03 10:51:10 +0900

    Request修正

--- Menu/resources/views/html/shop/RequestForm.blade.php
+++ Menu/resources/views/html/shop/RequestForm.blade.php
@@ -5,19 +5,22 @@
   @section('title','申請フォーム画面')
     <!--テキストサンプル-->
     @section('contents')
-    <form action="#" method="post"> 
-      <h2 class="Request">申請フォーム</h2> 
+
+    <form action="{{ route('insert.finish') }}" method="post"> 
+      <h2 class="Request">申請フォーム</h2>
+      <p>代表者</br>
+        <input type="text" name="leader" size="30">
       <p>店舗名</br>
-        <input type="text"  size="30">
+        <input type="text" name="name" size="30">
       </p>
       <p>住所</br>
-        <input type="text"  size="30">
+        <input type="text" name="adress" size="30">
       </p>
       <p>メールアドレス</br>
-        <input type="email"  size="30">
+        <input type="email" name="e-mail" size="30">
       </p>
       <p>電話番号</br>
-        <input type="tel" maxlength="12" size="30">
+        <input type="tel" name="tel" maxlength="12" size="30">
       </p>
       <button type="button" onclick=history.back()>戻る</button>
       <input type="submit">
