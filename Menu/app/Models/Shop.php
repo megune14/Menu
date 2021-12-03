111c6860d0f0c1c61f6348a82b4f4616a17a82f7
megune14 <megune1412@gmail.com>
2021-12-03 14:37:17 +0900

    修正

--- Menu/app/Models/Shop.php
+++ Menu/app/Models/Shop.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Menu extends Model
+class Shop extends Model
 {
     use HasFactory;
-    protected $fillable = ['id','name','price','shopid'];
+    protected $fillable = ['name'];
 }
