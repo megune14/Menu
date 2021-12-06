b01e6fcc021c7427c7e0302ee926a543d70053d8
kazusz2001 <mit2071082@stu.o-hara.ac.jp>
2021-12-06 11:44:10 +0900

    商品詳細設定画面をDB以外表示したよん

--- Menu/resources/views/html/shop/MenuDetail.blade.php
+++ Menu/resources/views/html/shop/MenuDetail.blade.php
@@ -7,9 +7,42 @@
 
   <!--テキストサンプル-->
   @section('contents')
+  <form action="#" method="post">
+  <span class="menudeta-img">
+        <img src="/images/men1.jpg"><!--写真-->
+  </span>
+  <p>商品名：
+	<input type="text" name="name"></p><br>
 
+  <p>値段：
+	<input type="number"  value="100" min="0" max="" step="10"></p>
+
+  <p>アレルギー：</p><br>
+  <p>カロリー</p><br>
 
   
+  <div class="confirm">
+    <button type="button" onclick=history.back()>戻る</button>
+    <input type="submit" value="変更" id="button4">
+  </div>
+
+  <label class="open" for="pop-up">変更</label>
+        <input type="checkbox" id="pop-up">
+        <div class="overlay">
+          <div class="window">
+            <label class="close" for="pop-up"></label>
+            <p class="text">メニュー情報を変更します。<br>
+            <font color="red">よろしいでしょうか？</font></p>            
+            <button type="button" onclick="history.back()">戻る</button>
+            <input type="submit" value="確定">
+          </div>
+          
+        </div>
+  </from>
+
+
+
+
 
     <!--/テキストサンプル-->
   @endsection
--- Menu/routes/web.php
+++ Menu/routes/web.php
@@ -73,5 +73,5 @@ Auth::routes(['verify' => true]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/MenuDetail',function () {
-    return view('html/user.MenuDetail');
+    return view('html/shop.MenuDetail');
 });
