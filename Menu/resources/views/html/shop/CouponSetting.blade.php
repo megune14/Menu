39e91c255f71ec593e55050adf2dd76c9b386538
kazusz2001 <mit2071082@stu.o-hara.ac.jp>
2021-12-06 15:25:55 +0900

    店舗側のクーポンの画面作ったー

--- Menu/resources/views/html/shop/CouponSetting.blade.php
+++ Menu/resources/views/html/shop/CouponSetting.blade.php
@@ -9,6 +9,21 @@
   @section('contents')
   <div class = CouponSetting>
   <h1>クーポン設定</h1>
+  <p>商品<br>
+     <select name="coupon">
+       <option value="">-</option>
+       <option value="醤油ラーメン">醤油ラーメン</option>
+       <option value="塩ラーメン">塩ラーメン</option>
+      </select>
+
+      <p>必要ポイント<br>
+	    <input type="number"  value="100" min="0" max="" step="1"></p>
+      <div class="confirm">
+            <button type="button" onclick=history.back()>戻る</button>
+            <input type="submit" value="確定" id="button5">
+      </div>
+
+      
 
   </div>
   
