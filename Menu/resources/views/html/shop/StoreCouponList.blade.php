815bcae5285986b5961a5420854bf413e955b666
shio-noodle <shiohata71156@yahoo.co.jp>
2021-12-10 11:34:55 +0900

    StoreCouponListのcss追加

--- Menu/resources/views/html/shop/StoreCouponList.blade.php
+++ Menu/resources/views/html/shop/StoreCouponList.blade.php
@@ -32,38 +32,43 @@
             <p>ハンバーガー</p>
           </div>
           
-          <p>必要ポイント数： 30pt</p>
+          <p>必要ポイント数: 30pt</p>
           
         </div>
         
 
         <div class="couponcard">
-          <tr>
-            <td><img src="/images/men1.jpg"></th><br>
-            <td>商品名</th><br>
-            <td>必要ポイント数：</td>
+
+          <img src="/images/men1.jpg"><br>
+
+          <div class="couponcheckbox">
+            <input type="checkbox" id="coupon-btn-check">
+            <label for="coupon-btn-check" class="coupon-btn"><span></span></label>
+
+            <div class="abc">
+              <ul>
+                <li><a href="#">変更</a></li>
+                <li><a href="#">削除</a></li>
+              </ul>
+
+            </div>   
+    
+          </div>
+
+          <div class="menuname">
+            <p>ハンバーガー</p>
+          </div>
+
+          <p>必要ポイント数: 30pt</p>
           
-            <div class="couponcheckbox">
-              <input type="checkbox" id="coupon-btn-check">
-              <label for="coupon-btn-check" class="coupon-btn"><span ></span></label>
-
-              <nav>
-                <ul>
-                  <li><a href="#">変更</a></li>
-                  <li><a href="#">削除</a></li>
-                </ul>
-              </nav>
-
-            </div>
-
-          </tr>
-
         </div>
         
-
-        <a id="coupon-send" href="">    
-          <input type="submit"  value="クーポンの追加"><!--送信ボタン-->
-        </a>
+        <div class="coupon-send">
+          <a id="coupon-send-submit" href="">    
+            <input type="submit"  value="クーポンの追加"><!--送信ボタン-->
+          </a>
+        </div>
+        
 
       </div> 
 
