7d83956b536839ca4c71f63eb264f45044bcf508
shio-noodle <shiohata71156@yahoo.co.jp>
2021-12-13 10:50:26 +0900

    RequestFormのcss追加

--- Menu/resources/views/html/shop/RequestForm.blade.php
+++ Menu/resources/views/html/shop/RequestForm.blade.php
@@ -6,27 +6,37 @@
     <!--テキストサンプル-->
     @section('contents')
 
-      <form action="RequestComplete.blade.php" method="post"> 
-        <h2 class="Request">申請フォーム</h2>
-        <p>代表者</br>
-        <input type="text" name="leader" size="30">
-        <p>店舗名</br>
-        <input type="text" name="name" size="30">
-        </p>
-        <p>住所</br>
-        <input type="text" name="address" size="30">
-        </p>
-        <p>メールアドレス</br>
-        <input type="email" name="email" size="30">
-        </p>
-        <p>電話番号</br>
-        <input type="tel" name="tel" maxlength="12" size="30">
-        </p>
-        <button type="button" onclick=history.back()>戻る</button>
-        <input type="submit">
-      </form>
+      <div class="request-form">
+
+        <form action="RequestComplete.blade.php" method="post"> 
+          <h2 class="Request">申請フォーム</h2>
+          <p>代表者　　　　
+           <input type="text" name="leader" size="30" placeholder="代表者">
+          </p>
+          <p>店舗名　　　　
+           <input type="text" name="name" size="30" placeholder="店舗名">
+          </p>
+          <p>住所　　　　　
+          <input type="text" name="address" size="30" placeholder="住所">
+          </p>
+          <p>メールアドレス
+          <input type="email" name="email" size="30" placeholder="メールアドレス">
+          </p>
+          <p>電話番号　　　
+          <input type="tel" name="tel" maxlength="12" size="30" placeholder="電話番号">
+          </p>
+
+          <div class="submit-request">
+            <button type="button" onclick=history.back()>戻る</button>
+            <input type="submit">
+          </div>
+          
+        </form>
+
+      </div>
+      
     
     <!--/テキストサンプル-->
-</body>
-</html>
+
+
   @endsection
