fb2cccbd0c51532113c62bbedab6a9b9e014db81
shio-noodle <shiohata71156@yahoo.co.jp>
2021-12-14 13:09:30 +0900

    PassChangeCheckのphp追加

--- Menu/routes/web.php
+++ Menu/routes/web.php
@@ -23,7 +23,7 @@ Route::get('/register',function () {
 
 /* お試し用ルート */
 Route::get('/a',function () {
-    return view('html/user.PassReSettingUserCheck');
+    return view('html/user.PassChangeCheck');
 });
 
 /* お試し用ルートその2 */
