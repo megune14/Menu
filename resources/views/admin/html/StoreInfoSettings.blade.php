5e4374fe9f8c49f6de4d23ab7ee559bca9dbb885
2-6gotou <mit2071036@stu.o-hara.ac.jp>
2022-01-18 14:20:40 +0900

    修正

--- resources/views/admin/html/StoreInfoSettings.blade.php
+++ resources/views/admin/html/StoreInfoSettings.blade.php
@@ -13,20 +13,31 @@
           <!--<div class="col-sm-10">-->
           @csrf
             
-          @foreach ($data as $tdata)
             <p>店舗名</br>
-            <input type="text" name="StoreName" value ="{{ $tdata->StoreName}}"></p>
+            <input type="text" name="shopname" value ="{{ $data->shopname}}"></p>
             <p>住所</br>
-            <input type="text" name="Adress" value ="{{ $tdata->Adress}}"></p>
+            <input type="text" name="address" value ="{{ $data->address}}"></p>
             <p>電話番号</br>
-            <input type="tel" name="TelephoneNumber" value ="{{ $tdata->TelephoneNumber}}"></p>
-          @endforeach
+            <input type="tel" name="tell" value ="{{ $data->tell}}"></p>
 
-          <input type = "hidden" name = "id" value = "{{$tdata->id}}">
+          <input type = "hidden" name = "id" value = "{{$data->id}}">
           <div class="submit-InfoSetting">
             <button type="button" onclick=history.back()>戻る</button>
-            <input type="submit" value="確定">
+            
+            <label class="open" for="pop-up" ><input type="submit" value="確定"></label>
+            <input type="checkbox" id="pop-up">
+          <div class="overlay">
+            <div class="window">
+              <label class="close" for="pop-up">×</label>
+              <p class="text">この商品を追加しますか<br>
+               </p>            
+              <button type="button" onclick="history.back()">戻る</button>
+              <input type="submit" value="確定">
+            </div>
+   
           </div>
+          </div>
+
 
         </form>
 
