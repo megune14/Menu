a6aff4de9dd3e0d97137ad658baeb4126b6679ae
shio-noodle <shiohata71156@yahoo.co.jp>
2022-01-19 15:42:26 +0900

    更新

--- resources/views/admin/html/CouponProduct.blade.php
+++ resources/views/admin/html/CouponProduct.blade.php
@@ -8,39 +8,39 @@
   <!--テキストサンプル-->
   @section('contents')
   <?php
-use App\Models\MenuFood;
-use App\Http\Controllers\MenuFoodController;
-//
-   
-$Coupon = MenuFood::get();
-
-?>
-  <form action="/admin/CouponProduct/add"  method='post'  >
-  @csrf
-  <div class = CouponProduct>
+  use App\Models\MenuFood;
+  use App\Http\Controllers\MenuFoodController;
+  //
     
+  $Coupon = MenuFood::get();
 
-  <div class="coupon-product">
-  <p>商品名：
-        <select name="name">
+  ?>
+    <form action="/admin/CouponProduct/add"  method='post'  >
+    @csrf
+      <div class = CouponProduct>
+      
+        <div class="coupon-product">
+
+          <p>商品名：
+            <select name="name">
+            
+            <?php foreach ($Coupon as $Coupon) {  ?>
+              <option  value="{{$Coupon->name }}">{{$Coupon->name }}</option>
+              <?php  } ?>
+            </select>
+          </p>
         
-        <?php foreach ($Coupon as $Coupon) {  ?>
-          <option  value="{{$Coupon->name }}">{{$Coupon->name }}</option>
-          <?php  } ?>
-        </select>
-        </p>
-      </div>
-  
-  
+          <p>必要ポイント：<input type="number" name="point" value="100" min="10" max="10000" step="10"></p>
 
-  <p>必要ポイント：<input type="number" name="point" value="100" min="10" max="10000" step="10"></p>
+        </div>
 
 
+        <div class="submit-CouponProduct">
+          <input type="submit"   value="確定"><!--送信ボタン-->
+        </div>
+        
+      </div>  
 
-  </div>
-
-  <input type="submit"   value="確定"><!--送信ボタン-->
-
-</form>
+    </form>
     <!--/テキストサンプル-->
   @endsection
