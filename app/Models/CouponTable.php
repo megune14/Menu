c6839297642cac196b9e30069332b3cc5bbcbedd
issei-ctrl <ozaki2071132@yahoo.co.jp>
2022-01-21 14:40:17 +0900

    コミット

--- app/Models/CouponTable.php
+++ app/Models/CouponTable.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class CouponTable extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+   
 }
