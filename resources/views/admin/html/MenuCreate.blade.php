0f989a658d225706a0c1b5802fea2d05fc1ea2e1
shio-noodle <shiohata71156@yahoo.co.jp>
2022-01-25 13:50:43 +0900

    一応完成

--- resources/views/admin/html/MenuCreate.blade.php
+++ resources/views/admin/html/MenuCreate.blade.php
@@ -9,38 +9,36 @@
   <!--テキストサンプル-->
   @section('contents')
 <h2>商品一覧</h2>
+<div class="search">
 {{csrf_field()}}
 {{ Form::open(['action' => 'MenuCreateController@create', 'method' => 'post']) }}
 {{ Form::select('FORM_NAME', CategoryTable::where('StoreID',Auth::id())->select('CategoryID', 'Category')->get()->pluck('Category','CategoryID')->prepend( "全て", "0"), null, ['class' => 'form-control']) }}
 {!! Form::submit('検索') !!}
 {{ Form::close() }}
+</div>
 @foreach ($Menu as $item)
 <div class="MenuCreate">
     <!--ここのURLにクエリパラメータで商品IDを送る-->
-    <a href="#">
+    
         <img src="{{ asset('images/men1.jpg') }}">
 
         <div class="Create-Name">
 
           <div class="Menucheckbox">
-              <input type="checkbox" id="Menubox"><!--データベースの数値-->
-              <label for="Menubox" class="Menu-btn"><span></span></label>
+              <input type="checkbox" id="Menubox{{$item->CommodityID}}"><!--データベースの数値-->
+              <label for="Menubox{{$item->CommodityID}}" class="Menu-btn"><span></span></label>
 
                 <div class="Menu-abc">
                   <ul>
                     <li><a href="/admin/login">変更</a></li>
                     <li><input type="submit"  value="削除"></li>
-                    <li><a href="#">販売中止</a></li>
-                    </form>
+                    <li><a href="/admin/login">販売中止</a></li>
                   </ul>
 
                 </div>
 
           </div>
           
-
-          
-
         
           <p>{{$item->CommodityName}}</p>
           
@@ -49,7 +47,7 @@
           </div>
 
         </div>   
-    </a>
+    
 </div>
 @endforeach
     <!--/テキストサンプル-->
