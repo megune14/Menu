a9de5fb2367734d4f0f87b19b73760c6e1c393c3
shio-noodle <shiohata71156@yahoo.co.jp>
2022-01-28 13:25:01 +0900

    更新

--- resources/views/admin/html/CouponDetail.blade.php
+++ resources/views/admin/html/CouponDetail.blade.php
@@ -10,31 +10,39 @@
 
     <div class="CouponDetail">
       <p>このクーポンを削除します。<br>　よろしいでしょうか？</p>
+
+
        
       <form action='/admin/CouponDetail/delete' method="POST" >
 
+        <div class="Detail-couponcard">
+
           {{ csrf_field() }}
 
-          <img src="{{ asset('storage/images/'.$commodity->img) }}" alt="">
+          <!--<img src="{{ asset('storage/images/'.$commodity->img) }}" alt="">-->
+          <img src="/images/men1.jpg" alt="">
 
-      <!-- ここにクーポンの情報がくるようにする-->
+        
 
 
-      <input type="hidden"  name="id" value="{{$data->CouponID}}">
-          <div class="menuname" name="name">
+          <input type="hidden"  name="id" value="{{$data->CouponID}}">
+
+          <div class="Detail-menuname" name="name">
             <p>{{$commodity->CommodityName}}</p>
           </div>
           <div class="menupoint" name="point">
-          <p>必要ポイント数： {{$data->Point}}pt</p>
+            <p>必要ポイント数：</p> <p>{{$data->Point}}pt</p>
           </div>
 
+        </div>  
 
 
-      <div class="submit-Detail">     
-        <button type="button" onclick="history.back()">戻る</button>
-        <input type="hidden"  name="id" value="{{$data->CouponID}}">
-        <input type="submit" value="確定" name='delete'>
-      </div>
+
+        <div class="submit-Detail">     
+          <button type="button" onclick="history.back()">戻る</button>
+          <input type="hidden"  name="id" value="{{$data->CouponID}}">
+          <input type="submit" value="確定" name='delete'>
+        </div>
 
 
 
