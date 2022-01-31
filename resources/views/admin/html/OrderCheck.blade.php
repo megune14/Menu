3c3102be52ffb5e90ca48d3b928ab12ba87130c4
megune14 <megune1412@gmail.com>
2022-01-31 16:19:31 +0900

    受注商品一覧画面完成

--- resources/views/admin/html/OrderCheck.blade.php
+++ resources/views/admin/html/OrderCheck.blade.php
@@ -9,6 +9,16 @@
         $("#submit_form").submit();
       });
     });
+    $(function(){
+  $("#js-tab li").click(function(){
+    $("#js-tab li").removeClass("active");
+    $(this).addClass("active");
+    var index = $(this).index();
+
+    $(".tab-content").removeClass("active");
+    $(".tab-content").eq(index).addClass("active");
+  });
+});
     </script>
 
 
@@ -16,37 +26,99 @@
 
   <!--テキストサンプル-->
   @section('contents')
-  <table>
-      <tr>
-          <th>状況</th>
-          <th>商品名</th>
-          <th>個数</th>
-          <th>卓番</th>
-      </tr>
-      @foreach ($allorderdetail as $order)
 
-      <tr>
-        <td><form method='post' action='/admin/OrderCheck/chenge' id='submit_form'>
-            @csrf
-            <select name="flag" onchange="submit(this.form)">
-                @if ($order->Flag === 0)
-                <option value="0" selected>未提供</option>
-                <option value="1">提供済み</option>
-                @else
-                <option value="0">未提供</option>
-                <option value="1" selected  >提供済み</option>
-                @endif
-            </select>
-            <input type="hidden" value="{{$order->OrderDetailID}}" name="id">
-        </form>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="wrap-tab">
+    <ul id="js-tab" class="list-tab">
+      <li class="active">タブ1</li>
+      <li>タブ2</li>
+    </ul>
 
-    </td>
-        <td>{{$order->CommodityName}}</td>
-        <td>{{$order->Quantity}}個</td>
-        <td>{{$order->TableNumber}}番</td>
+    <div class="wrap-tab-content">
+      <div class="tab-content active">
+        <table>
+            <tr>
+                <th>状況</th>
+                <th>商品名</th>
+                <th>個数</th>
+                <th>卓番</th>
+            </tr>
+            @foreach ($allorderdetail as $order)
+            @if ($order->Flag === 0)
 
-    </tr>
-        @endforeach
-</table>
+
+            <tr>
+              <td><form method='post' action='/admin/OrderCheck/chenge' id='submit_form'>
+                  @csrf
+                  <select name="flag" onchange="submit(this.form)">
+                      @if ($order->Flag === 0)
+                      <option value="0" selected>未提供</option>
+                      <option value="1">提供済み</option>
+                      @else
+                      <option value="0">未提供</option>
+                      <option value="1" selected  >提供済み</option>
+                      @endif
+                  </select>
+                  <input type="hidden" value="{{$order->OrderDetailID}}" name="id">
+              </form>
+
+          </td>
+              <td>{{$order->CommodityName}}</td>
+              <td>{{$order->Quantity}}個</td>
+              <td>{{$order->TableNumber}}番</td>
+
+          </tr>
+          @endif
+              @endforeach
+      </table>
+      </div>
+
+      <div class="tab-content">
+        <table>
+            <tr>
+                <th>状況</th>
+                <th>商品名</th>
+                <th>個数</th>
+                <th>卓番</th>
+            </tr>
+            @foreach ($allorderdetail as $order)
+            @if ($order->Flag === 1)
+
+
+            <tr>
+              <td><form method='post' action='/admin/OrderCheck/chenge' id='submit_form'>
+                  @csrf
+                  <select name="flag" onchange="submit(this.form)">
+                      @if ($order->Flag === 0)
+                      <option value="0" selected>未提供</option>
+                      <option value="1">提供済み</option>
+                      @else
+                      <option value="0">未提供</option>
+                      <option value="1" selected  >提供済み</option>
+                      @endif
+                  </select>
+                  <input type="hidden" value="{{$order->OrderDetailID}}" name="id">
+              </form>
+
+          </td>
+              <td>{{$order->CommodityName}}</td>
+              <td>{{$order->Quantity}}個</td>
+              <td>{{$order->TableNumber}}番</td>
+
+          </tr>
+          @endif
+              @endforeach
+      </table>
+      </div>
+
+    </div>
+  </div>
+
     <!--/テキストサンプル-->
   @endsection
