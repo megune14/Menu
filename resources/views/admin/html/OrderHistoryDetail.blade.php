282137f1d686cbb505fb1826e6b25785b09f09a4
megune14 <megune1412@gmail.com>
2022-02-03 14:05:13 +0900

    修正

--- resources/views/admin/html/OrderHistoryDetail.blade.php
+++ resources/views/admin/html/OrderHistoryDetail.blade.php
@@ -7,21 +7,36 @@
 
   <!--テキストサンプル-->
   @section('contents')
+  <script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+  <script type="text/javascript">
+
+$(function() {
+  $('#tehuda1 td button').mouseover( function(){
+    $('td').css('background-color','orange');
+  $('.sample1 button').css('background-color','orange');
+  });
+});
+</script>
   <br>
   <br>
   <br>
   <br>
   <br>
-  <table>
+  <table id='tehuda1'>
       <tr>
           <th>商品名</th>
           <th>個数</th>
         </tr>
         @foreach ($order1 as $order)
+        <tbody>
         <tr>
-            <td>{{$order->Commodity->CommodityName}}</td>
-            <td>{{$order->Quantity}}</td>
+            <td class="sample1"><button>{{$order->Commodity->CommodityName}}</td></button>
+            <td class="sample1"><button>{{$order->Quantity}}</button></td>
         </tr>
+    </tbody>
         @endforeach
 </table>
     <!--/テキストサンプル-->
