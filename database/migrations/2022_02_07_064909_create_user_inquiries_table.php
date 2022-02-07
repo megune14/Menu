cef6ada669fb68b99f4318329d179d0430f9640b
issei-ctrl <ozaki2071132@yahoo.co.jp>
2022-02-07 16:24:58 +0900

    userInruiry

---
+++ database/migrations/2022_02_07_064909_create_user_inquiries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInquiriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_inquiries', function (Blueprint $table) {
+            $table->id();
+            $table->string("inquiry");
+            $table->string("name");
+            $table->string("tell");
+            $table->string("email");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_inquiries');
+    }
+}
