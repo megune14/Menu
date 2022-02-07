e3180140d82ad79973777e73c04fc49a74ea2e48
megune14 <megune1412@gmail.com>
2022-02-07 17:08:30 +0900

    seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -18,7 +18,7 @@ class CategorySeeder extends Seeder
             [
               [
                 'CategoryID'=>1,
-                'StoreID' => 1 ,
+                'StoreID' => 1,
                 'Category' => '中華' ,
               ],
               [
@@ -33,13 +33,53 @@ class CategorySeeder extends Seeder
               ],
               [
                 'CategoryID'=>4,
-                'StoreID' => 1 ,
+                'StoreID' => 2 ,
                 'Category' => '麺' ,
               ],
               [
                 'CategoryID'=>5,
-                'StoreID' => 1 ,
+                'StoreID' => 2 ,
                 'Category' => 'そば' ,
+              ],
+              [
+                'CategoryID'=>6,
+                'StoreID' => 2 ,
+                'Category' => '酒' ,
+              ],
+              [
+                'CategoryID'=>7,
+                'StoreID' => 3 ,
+                'Category' => 'ジュース' ,
+              ],
+              [
+                'CategoryID'=>8,
+                'StoreID' => 3 ,
+                'Category' => '丼もの' ,
+              ],
+              [
+                'CategoryID'=>9,
+                'StoreID' => 3 ,
+                'Category' => 'うどん' ,
+              ],
+              [
+                'CategoryID'=>10,
+                'StoreID' => 3 ,
+                'Category' => 'パスタ' ,
+              ],
+              [
+                'CategoryID'=>11,
+                'StoreID' => 3 ,
+                'Category' => '鍋' ,
+              ],
+              [
+                'CategoryID'=>12,
+                'StoreID' => 3 ,
+                'Category' => 'イタリアン' ,
+              ],
+              [
+                'CategoryID'=>13,
+                'StoreID' => 3,
+                'Category' => 'ピザ' ,
               ],
             ],
         );
