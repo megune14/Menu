229866ecb8bf6b47683289672a859ecc347391bf
megune14 <megune1412@gmail.com>
2022-02-08 10:07:22 +0900

    cart

--- resources/views/user/html/MOBILEbase.blade.php
+++ resources/views/user/html/MOBILEbase.blade.php
@@ -14,10 +14,10 @@
     <!--header-->
     <header>
       <div class="header">
-       <li><p>デジタルメニュー表</p><img src="../../images/カート.png"></img></li>
+       <li><p>デジタルメニュー表</p><a href="{{route(cart.index)}}"><img src="../../images/カート.png"></a></img></li>
       </div>
 
-      
+
 
       <div class="menu">
         <input type="checkbox" id="menu-btn-check">
@@ -52,7 +52,7 @@
           </ul>
         </nav>
 
-        
+
 
       </div>
     </header>
