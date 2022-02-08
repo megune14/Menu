6561c9f232f8099c1d01a8764b181557283c1572
issei-ctrl <ozaki2071132@yahoo.co.jp>
2022-02-08 10:12:13 +0900

    syuusei

--- resources/views/admin/html/OrderCheck.blade.php
+++ resources/views/admin/html/OrderCheck.blade.php
@@ -57,7 +57,6 @@
         <li class="pc-child-Product"><a href="#">設定</a>
           <ul>
             <li><a href="/admin/MailChange">メールアドレス変更</a></li>
-            <li><a href="/admin/PassChangeCheck">パスワード変更</a></li>
             <li><a href="/admin/PointSetting">ポイント設定</a></li>
             <li><a href="/admin/StoreInfoDetail">　店舗情報</a></li>
           </ul>
@@ -87,7 +86,6 @@
             <li class="has-child"><a href="#">　設定</a>
               <ul>
                 <li><a href="/admin/MailChange">　メールアドレス変更</a></li>
-                <li><a href="/admin/PassChangeCheck">　パスワード変更</a></li>
                 <li><a href="/admin/PointSetting">　ポイント設定</a></li>
                 <li><a href="/admin/StoreInfoDetail">　店舗情報</a></li>
               </ul>
