36112945f7983bb688467c8c6a08e5f938dd1898
shio-noodle <shiohata71156@yahoo.co.jp>
2022-02-09 12:39:29 +0900

    更新

--- resources/views/user/html/MOBILEbase.blade.php
+++ resources/views/user/html/MOBILEbase.blade.php
@@ -14,7 +14,7 @@
     <!--header-->
     <header>
       <div class="header">
-       <li><p>デジタルメニュー表</p><img src="../../images/カート.png"></img></li>
+       <li><p>デジタルメニュー表</p><a href="/cart"><img src="../../images/カート.png"></img></a></li>
       </div>
 
       
